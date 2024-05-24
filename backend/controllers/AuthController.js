c86fbc959a678af284b79197ab960b61a198cbac
haparece <harvey@fulloutsoftware>
2024-05-25 02:03:38 +0800

    Setting up access and refresh tokens with jwt

--- backend/controllers/AuthController.js
+++ backend/controllers/AuthController.js
@@ -1,5 +1,6 @@
 const User = require('../models/User');
 const bcryptJS = require('bcryptjs');
+const jwt = require('jsonwebtoken');
 
 module.exports = {
 	login: async (req, res) => {
@@ -12,8 +13,17 @@ module.exports = {
 
 		if (user) {
 			if (await bcryptJS.compare(req.body.password, user.password)) {
+
+				// generate access token and refresh token
+				const accessToken = jwt.sign({ user_id: user.id, refresh: false }, process.env.SECRET_KEY, { expiresIn: process.env.TOKEN_EXP });
+				const refressToken = jwt.sign({ user_id: user.id, refresh: true }, process.env.REFRESH_TOKEN_KEY, { expiresIn: process.env.REFRESH_TOKEN_EXP });
+
 				response.status = 200;
 				response.message = 'Successfully login!';
+				response.data = {
+					access: accessToken,
+					refresh: refressToken
+				}
 			}
 		}
 
@@ -39,7 +49,36 @@ module.exports = {
 			response.status = 200;
 			response.data = { user };
 		} catch (e) {
-			response.message = response.message + ' ' + e.message;
+			response.message = response.message + ' => ' + e.message;
+		}
+
+		res.status(response.status).json(response);
+	},
+
+	refresh: async (req, res) => {
+		const response = {
+			status: 401,
+			data: {},
+			message: 'Invalid Refresh Token'
+		};
+
+		try {
+			const data = jwt.verify(req.body.refresh, process.env.REFRESH_TOKEN_KEY);
+
+			if (data && data.refresh) {
+				// generate new access and refresh token
+				const accessToken = jwt.sign({ user_id: data.user_id, refresh: false }, process.env.SECRET_KEY, { expiresIn: process.env.TOKEN_EXP });
+				const refressToken = jwt.sign({ user_id: data.user_id, refresh: true }, process.env.REFRESH_TOKEN_KEY, { expiresIn: process.env.REFRESH_TOKEN_EXP });
+
+				response.status = 200;
+				response.message = 'Successfully refresh tokens';
+				response.data = {
+					access: accessToken,
+					refresh: refressToken
+				}
+			}
+		} catch (e) {
+			response.message = response.message + ' => ' + e.message;
 		}
 
 		res.status(response.status).json(response);
--- backend/routes/AuthRoutes.js
+++ backend/routes/AuthRoutes.js
@@ -1,8 +1,8 @@
 const router = require('express').Router();
 const AuthController = require('../controllers/AuthController');
 
-
 router.post('/login', AuthController.login);
 router.post('/register', AuthController.register);
+router.post('/token/refresh', AuthController.refresh);
 
 module.exports = router;
