6f6fb5b4dc219c33405c7c618607284bb2256d0c
acerapa <harvey.aparece.work@gmail.com>
2024-06-09 03:32:17 +0800

    Hot Fixes

--- backend/models/index.js
+++ backend/models/index.js
@@ -8,7 +8,7 @@ const basename = path.basename(__filename);
 const env = process.env.NODE_ENV || 'development';
 const config = require(__dirname + '/../config/config')['development']; // force to use development configs
 const db = {};
-const { migrator } = require('./global/helper');
+const { migrator } = require('../global/helper');
 
 let sequelize = new Sequelize(config.database, config.username, config.password, config);
 
