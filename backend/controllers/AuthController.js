51a3ac84029a2816c88ebe67572fca0bf870e40a
acerapa <harvey.aparece.work@gmail.com>
2024-06-08 20:53:53 +0800

    Import user model naming change

--- backend/controllers/AuthController.js
+++ backend/controllers/AuthController.js
@@ -1,4 +1,4 @@
-const User = require('../models/user');
+const User = require('../models/User');
 const bcryptJS = require('bcryptjs');
 const jwt = require('jsonwebtoken');
 
--- backend/migrations/20240521084241-User.js
+++ backend/migrations/20240521084241-User.js
@@ -1,6 +1,6 @@
 'use strict';
 
-const User = require('../models/user');
+const User = require('../models/User');
 
 /** @type {import('sequelize-cli').Migration} */
 module.exports = {
