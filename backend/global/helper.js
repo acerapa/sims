24852d44523d0798cfa57093db1ebe972b89efac
acerapa <harvey.aparece.work@gmail.com>
2024-06-09 10:04:07 +0800

    Testing api config if has values

--- backend/global/helper.js
+++ backend/global/helper.js
@@ -1,14 +1,10 @@
-// const path = require('path');
-// const { Umzug, SequelizeStorage } = require('umzug');
-// const { sequelize } = require('../models');
-
 const execSync = require('child_process').execSync;
 
 module.exports = {
 	migrator: async () => {
 			return new Promise((resolve, reject) => {
 				try {
-					execSync('node_modules/.bin/sequelize db:migrate', { stdio: 'inherit' });
+					execSync(__dirname + '../../node_modules/.bin/sequelize db:migrate', { stdio: 'inherit' });
 					resolve('OK');
 				} catch (e) {
 					reject(e);
