8ca8c27af6567563974eab663c042d57831ba8a6
acerapa <harvey.aparece.work@gmail.com>
2024-06-11 20:11:21 +0800

    Frontend of the supplier module

---
+++ frontend/src/components/Vendor/VendorModal.vue
@@ -0,0 +1,156 @@
+<template>
+  <ModalWrapper
+    :title="title"
+    v-model="showModal"
+    @submit="onSubmit"
+  >
+    <div class="flex flex-col gap-4 my-7">
+      <div class="flex flex-col gap-3">
+        <p class="text-base font-semibold">Basic Info</p>
+        <div class="flex gap-6">
+          <input
+            type="text"
+            class="input flex-1"
+            placeholder="Company Name"
+            v-model="model.company_name"
+          />
+          <select class="input flex-1" v-model="model.annotation">
+            <option value="" hidden>Select annotation</option>
+            <option value="Mr.">Mr.</option>
+            <option value="Ms.">Ms.</option>
+            <option value="Mrs.">Mrs.</option>
+          </select>
+        </div>
+        <div class="flex gap-6">
+          <input
+            type="text"
+            class="input flex-1"
+            placeholder="First Name"
+            v-model="model.first_name"
+          />
+          <input
+            type="text"
+            class="input flex-1"
+            placeholder="Last Name"
+            v-model="model.last_name"
+          />
+        </div>
+      </div>
+      <div class="flex flex-col gap-3">
+        <p class="text-base font-semibold">Address Info</p>
+        <div class="flex flex-col gap-3">
+          <input
+            type="text"
+            class="input w-full"
+            placeholder="Address 1"
+            v-model="model.address.address1"
+          />
+          <input
+            type="text"
+            class="input w-full"
+            placeholder="Address 2"
+            v-model="model.address.address2"
+          />
+        </div>
+        <div class="flex gap-6">
+          <input
+            type="text"
+            class="input flex-1"
+            placeholder="City"
+            v-model="model.address.city"
+          />
+          <input
+            type="text"
+            class="input flex-1"
+            placeholder="Zip Code"
+            v-model="model.address.postal"
+          />
+        </div>
+      </div>
+      <div class="flex flex-col gap-3">
+        <p class="text-base font-semibold">Contact Info</p>
+        <div class="flex gap-6">
+          <input
+            type="text"
+            class="input flex-1"
+            placeholder="Mobile Number"
+            v-model="model.phone"
+          />
+          <input
+            type="text"
+            class="input flex-1"
+            placeholder="Telephone Number"
+            v-model="model.telephone"
+          />
+        </div>
+        <div class="flex gap-6">
+          <input
+            type="text"
+            class="input flex-1"
+            placeholder="Email"
+            v-model="model.email"
+          />
+          <input
+            type="text"
+            class="input flex-1"
+            placeholder="Fax"
+            v-model="model.fax"
+          />
+        </div>
+      </div>
+    </div>
+  </ModalWrapper>
+</template>
+
+<script setup>
+import { Method, authenticatedApi } from "@/api";
+import ModalWrapper from "@/components/wrappers/ModalWrapper.vue";
+import { useVendorStore } from "@/stores/supplier";
+import { onMounted, ref } from "vue";
+
+const showModal = defineModel();
+
+const props = defineProps({
+  isEdit: {
+    type: Boolean,
+    default: false,
+  },
+	selectedId: {
+		type: Number,
+		required: true
+	}
+});
+
+const supplierStore = useVendorStore();
+const title = ref(props.isEdit ? 'Edit Vendor/Supplier' : 'New Vendor/Supplier');
+const apiPath = ref(props.isEdit ? "suppliers/update" : "suppliers/register");
+const model = ref({
+  company_name: "",
+  first_name: "",
+  last_name: "",
+  annotation: "",
+  phone: "",
+  email: "",
+  telephone: "",
+  fax: "",
+  address: {
+    address1: "",
+    address2: "",
+    city: "",
+    postal: "",
+  },
+});
+
+const onSubmit = async () => {
+  await authenticatedApi(apiPath.value, Method.POST, model.value);
+	await supplierStore.fetchAllSuppliers();
+
+	showModal.value = false;
+};
+
+onMounted(() => {
+	if (props.isEdit && props.selectedId) {
+		model.value = supplierStore.suppliers.find(sup => sup.id == props.selectedId);
+	}
+});
+</script>
--- frontend/src/views/Vendor.vue
+++ frontend/src/views/Vendor.vue
@@ -1,3 +1,99 @@
 <template>
-	<div>Vendor/Supplier</div>
+  <div>
+    <VendorModal
+      v-if="showModal"
+      v-model="showModal"
+      :is-edit="isEdit"
+      :selected-id="toEdit"
+    />
+    <DeleteConfirmModal
+      v-model="showDeleteConfirmModal"
+      v-if="showDeleteConfirmModal"
+      :href="'suppliers/delete'"
+      :data="toDelete"
+      @after-delete="afterDelete"
+    />
+    <div class="table-wrapper w-full">
+      <div class="flex justify-between items-center">
+        <input
+          type="search"
+          class="input w-full max-w-72"
+          placeholder="Search"
+        />
+        <button
+          class="bg-primary p-2 rounded"
+          @click="
+            showModal = true;
+            isEdit = false;
+          "
+        >
+          <img src="../assets//icons/plus.svg" alt="Plus" />
+        </button>
+      </div>
+      <hr class="bg-gray-50 -mx-4" />
+      <div class="flex flex-col gap-7 overflow-x-auto pb-5">
+        <div class="grid grid-cols-7 gap-3 items-center min-w-[564px]">
+          <div class="col-span-1 flex gap-3 items-center">
+            <input type="checkbox" class="input" />
+            <p class="table-header">#</p>
+          </div>
+          <p class="col-span-3 table-header">Company Name</p>
+          <p class="col-span-2 table-header">Rep. Name</p>
+          <p class="col-span-1 table-header">Action</p>
+        </div>
+        <div class="flex flex-col gap-4 min-w-[564px]">
+          <VendorRow
+            v-for="(supplier, ndx) in vendorStore.suppliers"
+            :supplier="supplier"
+            :key="ndx"
+            :current-open-menu="selectedMenuRow"
+            @open-menu="onSelectRow"
+            @view="onViewRow"
+            @delete="onDeleteRow"
+          />
+        </div>
+      </div>
+    </div>
+  </div>
 </template>
+
+<script setup>
+import { onMounted, ref } from "vue";
+import DeleteConfirmModal from "@/components/DeleteConfirmModal.vue";
+import VendorModal from "@/components/Vendor/VendorModal.vue";
+import VendorRow from "@/components/Vendor/VendorRow.vue";
+import { useVendorStore } from "@/stores/supplier";
+
+const vendorStore = useVendorStore();
+
+const showModal = ref(false);
+const isEdit = ref(false);
+const toEdit = ref(-1);
+const toDelete = ref();
+const selectedMenuRow = ref(-1);
+const showDeleteConfirmModal = ref(false);
+
+const onSelectRow = (id) => {
+  selectedMenuRow.value = id;
+};
+
+const onViewRow = (id) => {
+  toEdit.value = id;
+  isEdit.value = true;
+  showModal.value = true;
+};
+
+const onDeleteRow = async (id) => {
+  toDelete.value = { id };
+  showDeleteConfirmModal.value = true;
+};
+
+const afterDelete = async () => {
+  await vendorStore.fetchAllSuppliers();
+  showDeleteConfirmModal.value = false;
+}
+
+onMounted(async () => {
+  await vendorStore.fetchAllSuppliers();
+});
+</script>
