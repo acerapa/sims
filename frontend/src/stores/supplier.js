b60a5fe6fd98b6cca8e5e3c0f075c42934efa9f0
acerapa <harvey.aparece.work@gmail.com>
2024-08-29 02:38:49 +0800

    Initial update

--- frontend/src/stores/supplier.js
+++ frontend/src/stores/supplier.js
@@ -1,22 +1,33 @@
-import { authenticatedApi } from '@/api';
-import { defineStore } from 'pinia';
-import { ref } from 'vue';
+import { authenticatedApi } from "@/api";
+import { defineStore } from "pinia";
+import { ref } from "vue";
 
-export const useVendorStore = defineStore('supplier', () => {
-const suppliers = ref([]);
+export const useVendorStore = defineStore("supplier", () => {
+  const suppliers = ref([]);
 
-const selectedSupplier = ref();
+  const selectedSupplier = ref();
 
-const fetchAllSuppliers = async () => {
-	const res = await authenticatedApi('suppliers/all');
-	if (res.status == 200) {
-		suppliers.value = res.data.suppliers;
-	}
-}
+  const fetchAllSuppliers = async () => {
+    const res = await authenticatedApi("suppliers/all");
+    if (res.status == 200) {
+      suppliers.value = res.data.suppliers;
+    }
+  };
 
-return {
-	suppliers,
-	selectedSupplier,
-	fetchAllSuppliers
-}
+  const getSupplierOptions = async () => {
+    await fetchAllSuppliers();
+    return suppliers.value.map((sup) => {
+      return {
+        value: sup.id,
+        text: sup.company_name,
+      };
+    });
+  };
+
+  return {
+    suppliers,
+    selectedSupplier,
+    fetchAllSuppliers,
+		getSupplierOptions,
+  };
 });
