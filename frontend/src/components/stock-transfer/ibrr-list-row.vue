ecf08605f0f62c1fec67be4a655057d3ba5fa7a7
acerapa <harvey.aparece.work@gmail.com>
2024-11-13 13:09:37 +0800

    Added process by column in the stock transfer lists

--- frontend/src/components/stock-transfer/ibrr-list-row.vue
+++ frontend/src/components/stock-transfer/ibrr-list-row.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="grid grid-cols-8 gap-3 gen-table-row">
+  <div class="grid grid-cols-9 gap-3 gen-table-row">
     <div class="col-span-1 flex gap-3 items-center">
       <input type="checkbox" class="input" />
       <p class="text-sm">{{ props.ibrr.id }}</p>
@@ -7,6 +7,7 @@
     <p class="col-span-1 text-sm truncate">{{ props.ibrr.str_id }}</p>
     <p class="col-span-2 text-sm truncate">{{ props.ibrr.sender.name }}</p>
     <p class="col-span-1 text-sm truncate">{{ managerName }}</p>
+    <p class="col-span-1 text-sm truncate">{{ processByName }}</p>
     <p class="col-span-2 text-sm truncate">
       {{ DateHelpers.formatDate(props.ibrr.when, 'MM/DD/YYYY HH:II a') }}
     </p>
@@ -37,6 +38,11 @@ const props = defineProps({
 const managerName = computed(() => {
   const { sender } = props.ibrr
   return `${sender.manager.first_name} ${sender.manager.last_name}`
+})
+
+const processByName = computed(() => {
+  const { process_by } = props.ibrr
+  return `${process_by.first_name} ${process_by.last_name}`
 })
 
 const openMenu = (id) => {
--- frontend/src/components/stock-transfer/str-list-row.vue
+++ frontend/src/components/stock-transfer/str-list-row.vue
@@ -1,11 +1,12 @@
 <template>
-  <div class="grid grid-cols-7 gap-3 gen-table-row">
+  <div class="grid grid-cols-8 gap-3 gen-table-row">
     <div class="col-span-1 flex gap-3 items-center">
       <input type="checkbox" class="input" />
       <p class="text-sm">{{ props.str.id }}</p>
     </div>
     <p class="col-span-2 text-sm truncate">{{ props.str.receiver.name }}</p>
     <p class="col-span-1 text-sm truncate">{{ managerName }}</p>
+    <p class="col-span-1 text-sm truncate">{{ processByName }}</p>
     <p class="col-span-2 text-sm truncate">
       {{ DateHelpers.formatDate(props.str.when, 'MM/DD/YYYY HH:II a') }}
     </p>
@@ -35,6 +36,11 @@ const props = defineProps({
 const managerName = computed(() => {
   const { receiver } = props.str
   return `${receiver.manager.first_name} ${receiver.manager.last_name}`
+})
+
+const processByName = computed(() => {
+  const { process_by } = props.str
+  return `${process_by.first_name} ${process_by.last_name}`
 })
 
 const openMenu = (id) => {
