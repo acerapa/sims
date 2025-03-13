6b68487f13ec48d5faa6ef6366b203d0cb32a824
acerapa <harvey.aparece.work@gmail.com>
2025-03-13 17:08:42 +0800

    STR validating product quantity to product quantity on hand.

--- frontend/src/helper.js
+++ frontend/src/helper.js
@@ -7,3 +7,22 @@ export const getCost = (cost, prd, sup_id) => {
       ? sup.ProductSupplier.cost
       : prd.cost
 }
+
+export function validateProductStocks(productsModel, products) {
+  const productModelIds = productsModel.map((pm) => pm.product_id)
+  const productsInvolve = products
+    .filter((p) => productModelIds.includes(p.id))
+    .map((p) => ({ id: p.id, stock: p.product_details.stock }))
+
+  const errors = {}
+
+  productsModel.forEach((pm) => {
+    if (
+      pm.quantity > productsInvolve.find((p) => p.id == pm.product_id).stock
+    ) {
+      errors[pm.product_id] = 'Not enough stock'
+    }
+  })
+
+  return Object.keys(errors).length > 0 ? errors : null
+}
