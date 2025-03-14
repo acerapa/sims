a6cfcc46c4c9740d9d9ca1cf6a8c809136361870
acerapa <harvey.aparece.work@gmail.com>
2025-03-15 02:24:18 +0800

    customer seeder

---
+++ backend/seeders/20250314110131-customer-seeder.js
@@ -0,0 +1,60 @@
+"use strict";
+const { basename } = require("path");
+const Customer = require("../models/customer");
+const {
+  checkIfSeederExecuted,
+  registerSeederExecution,
+  getSeederExecution,
+  removeSeederExecution,
+} = require("./misc/SeederHelpers");
+const { ConsoleColors } = require("shared/enums");
+const customerDummy = require("./dummy/customers");
+const { Op } = require("sequelize");
+const { removeConstraints } = require("../models");
+const Address = require("../models/address");
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    const isExecuted = await checkIfSeederExecuted(basename(__filename));
+    console.log(
+      `${ConsoleColors.BLUE}Is Executed: ${isExecuted}${ConsoleColors.RESET}`
+    );
+    if (!isExecuted) {
+      const res = await Customer.bulkCreate(customerDummy, {
+        include: [
+          {
+            model: Address,
+            as: "address",
+          },
+        ],
+      });
+      console.log(
+        `${ConsoleColors.GREEN}Seeder Executed: ${res}${ConsoleColors.RESET}`
+      );
+      await registerSeederExecution(
+        basename(__filename),
+        res.map((c) => c.id),
+        true
+      );
+    }
+  },
+
+  async down(queryInterface, Sequelize) {
+    // remove constaints
+    await removeConstraints(Customer.getTableName(), queryInterface);
+
+    const seeder = await getSeederExecution(basename(__filename));
+    if (seeder) {
+      await Customer.destroy({
+        where: {
+          id: {
+            [Op.in]: seeder.data,
+          },
+        },
+      });
+
+      await removeSeederExecution(basename(__filename));
+    }
+  },
+};
