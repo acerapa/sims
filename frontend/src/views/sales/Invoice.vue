47cb8c43bb5dd2d621be6d22d67cf5d280859af7
acerapa <harvey.aparece.work@gmail.com>
2025-03-28 10:03:36 +0800

    Initial create invoice changes

--- frontend/src/views/sales/Invoice.vue
+++ frontend/src/views/sales/Invoice.vue
@@ -1,11 +1,34 @@
 <template>
-  <CustomTable>
+  <CustomTable @add-new-record="onAddNew">
     <template #table_header>
-      <div class="grid grid-cols-8"></div>
+      <div class="grid gap-3 grid-cols-7 min-w-[985px]">
+        <div class="col-span-1 flex gap-3 items-center">
+          <input type="checkbox" class="input" />
+          <p class="table-header">#</p>
+        </div>
+        <p class="col-span-2 table-header">Customer</p>
+        <p class="col-span-1 table-header">Issue Date</p>
+        <p class="col-span-1 table-header">Due Date</p>
+        <p class="col-span-1 table-header">Payment Method</p>
+        <p class="col-span-1 table-header">Total</p>
+      </div>
     </template>
   </CustomTable>
 </template>
 
 <script setup>
 import CustomTable from '@/components/shared/CustomTable.vue'
+import { SalesConst } from '@/const/route.constants'
+import { useRouter } from 'vue-router'
+
+const router = useRouter()
+
+/** ================================================
+ * METHODS
+ ** ================================================*/
+const onAddNew = () => {
+  router.push({
+    name: SalesConst.INVOICE_FORM
+  })
+}
 </script>
