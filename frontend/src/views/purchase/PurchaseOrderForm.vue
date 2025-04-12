d87d74cca6cd89fc0ea870943ba279e503ef68fa
acerapa <harvey.aparece.work@gmail.com>
2025-04-12 10:37:18 +0800

    Remove console log

--- frontend/src/views/purchase/PurchaseOrderForm.vue
+++ frontend/src/views/purchase/PurchaseOrderForm.vue
@@ -420,7 +420,6 @@ const onSubmit = async (isAddNew = false) => {
   // validation
   validateData()
   if (hasErrors.value) {
-    console.log(errors.value)
     Event.emit(rowEventName, errors.value.products)
     return
   }
