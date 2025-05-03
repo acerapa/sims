d78f50b22010834eceaf31e3be4482f466317d58
acerapa <harvey.aparece.work@gmail.com>
2025-05-03 15:26:13 +0800

    Setting up the receive payment list

--- frontend/src/stores/received-payments.js
+++ frontend/src/stores/received-payments.js
@@ -1,5 +1,6 @@
 import { api, Method } from '@/api'
 import { defineStore } from 'pinia'
+import { ref } from 'vue'
 
 export const useReceivedPaymentsStore = defineStore('received-payments', () => {
   const receivedPayments = ref([])
@@ -17,9 +18,32 @@ export const useReceivedPaymentsStore = defineStore('received-payments', () => {
     return isSuccess
   }
 
+  const fetchReceivedPayments = async () => {
+    const res = await api('/received-payments')
+
+    const isSuccess = res.status < 400
+
+    if (isSuccess) {
+      receivedPayments.value = res.data.received_payments
+    }
+
+    return isSuccess
+  }
+
+  // getters
+  const getReceivedPayments = async () => {
+    if (!receivedPayments.value.length) {
+      await fetchReceivedPayments()
+    }
+
+    return receivedPayments.value
+  }
+
   return {
     receivedPayments,
 
+    getReceivedPayments,
+    fetchReceivedPayments,
     registerReceivedPayment
   }
 })
