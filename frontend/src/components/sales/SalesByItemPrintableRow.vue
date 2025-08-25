2b537fe4b6d3c5c5376bf6efdce13d31ce1f8cc6
haparece-fc <haparece@fullscale.ph>
2025-08-25 23:37:43 +0800

    Fix sales by item detailed report printable

--- frontend/src/components/sales/SalesByItemPrintableRow.vue
+++ frontend/src/components/sales/SalesByItemPrintableRow.vue
@@ -15,41 +15,51 @@
         />
       </div>
     </div>
-    <div
-      class="col-span-full"
-      v-if="props.sales[1].products"
-      v-for="product in props.sales[1].products"
-    >
-      <div class="grid grid-cols-15 gap-3" v-for="invoice in product.invoices">
-        <p class="col-span-3" />
-        <p class="col-span-2 text-sm">
-          {{
-            DateHelpers.formatDate(new Date(invoice.issue_date), 'YYYY-MM-DD')
-          }}
-        </p>
-        <p class="col-span-1 text-sm">{{ product.id }}</p>
-        <p class="col-span-3 text-sm">
-          {{ invoice.memo || '-' }}
-        </p>
-        <p class="col-span-3 text-sm">
-          {{ product.product_details.sales_description }}
-        </p>
-        <p class="col-span-1 text-sm text-center">
-          {{ invoice.InvoiceProducts.quantity }}
-        </p>
-        <p class="col-span-1 text-sm text-end">
-          {{ invoice.InvoiceProducts.price }}
-        </p>
-        <p class="col-span-1 text-sm text-end">
-          {{ invoice.InvoiceProducts.total }}
-        </p>
+    <div class="col-span-full">
+      <div
+        class="group"
+        v-if="props.sales[1].products"
+        v-for="product in props.sales[1].products"
+      >
+        <div
+          class="grid grid-cols-15 gap-3"
+          v-for="invoice in product.invoices"
+        >
+          <p class="col-span-3" />
+          <p class="col-span-2 text-sm">
+            {{
+              DateHelpers.formatDate(new Date(invoice.issue_date), 'YYYY-MM-DD')
+            }}
+          </p>
+          <p class="col-span-1 text-sm">{{ product.id }}</p>
+          <p class="col-span-3 text-sm">
+            {{ invoice.memo || '-' }}
+          </p>
+          <p class="col-span-3 text-sm">
+            {{ product.product_details.sales_description }}
+          </p>
+          <p class="col-span-1 text-sm text-center">
+            {{ invoice.InvoiceProducts.quantity }}
+          </p>
+          <p class="col-span-1 text-sm text-end">
+            {{ invoice.InvoiceProducts.price }}
+          </p>
+          <p
+            class="col-span-1 text-sm text-end border-black group-last:border-b"
+          >
+            {{ invoice.InvoiceProducts.total }}
+          </p>
+        </div>
       </div>
     </div>
     <p
       :style="{ paddingLeft: `${props.depth * 12}px` }"
-      class="col-span-full text-sm font-bold"
+      class="col-span-14 text-sm font-bold"
     >
       Total {{ props.sales[0] }}
+    </p>
+    <p class="col-span-1 text-end text-sm font-bold">
+      {{ getTotalAmount().toFixed(2) }}
     </p>
   </div>
 </template>
@@ -67,4 +77,31 @@ const props = defineProps({
     default: 0
   }
 })
+
+const getTotalAmount = (s = props.sales[1]) => {
+  let total = 0
+
+  const getTotal = (s) => {
+    const salesKeys = Object.keys(s)
+
+    for (const key of salesKeys) {
+      if (key === 'products') {
+        total += s.products
+          .map((product) =>
+            product.invoices.map((invoice) =>
+              parseFloat(invoice.InvoiceProducts.total)
+            )
+          )
+          .flat()
+          .reduce((a, b) => a + b, 0)
+      } else {
+        getTotal(s[key])
+      }
+    }
+  }
+
+  getTotal(s)
+
+  return total
+}
 </script>
